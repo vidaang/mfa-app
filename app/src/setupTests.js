03745555ea9e336d54fafbafe6a2176ebf223970
vidaang <ViNTDang@gmail.com>
2026-08-15 17:27:37 -0700

    chore(tests): add test setup

--- app/src/setupTests.js
+++ app/src/setupTests.js
@@ -3,3 +3,16 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Polyfills for TextEncoder/TextDecoder used by some dependencies (MUI/data-grid)
+try {
+	if (typeof TextEncoder === 'undefined') {
+		// Node.js provides util.TextEncoder in newer versions
+		// eslint-disable-next-line global-require
+		const { TextEncoder, TextDecoder } = require('util');
+		global.TextEncoder = TextEncoder;
+		global.TextDecoder = TextDecoder;
+	}
+} catch (e) {
+	// ignore
+}
